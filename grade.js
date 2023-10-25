8a938f3b52095b9c647e9af1fe80717e8185d6e8
turkmenyigit <egarlyyev@yahoo.com>
2023-10-25 10:05:31 +0300

    by using for and if-else statement

---
+++ grade.js
@@ -0,0 +1,16 @@
+var n=-12;
+for(i=0; i<1; i++){
+    if (n<20){
+        console.log("poor")
+    }else if(n<40){
+        console.log("fail")
+    }else if(n<60){
+        console.log("middle")
+    }else if(n<80){
+        console.log("pass")
+    }else if(n<=100){
+        console.log("Successfull")
+    }else{
+        console.log("You're too way much")
+    }
+}
